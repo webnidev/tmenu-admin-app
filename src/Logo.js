53de4417df1fdd098c2c1011bf70e24d57ec56ad
Barroso Filho <barrosofilho@MacBook-Pro-de-Barroso.local>
2020-11-23 23:59:39 -0300

    Organização dos componentes por pasta

---
+++ src/Logo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Logo = () => {
+    return (
+        <>    
+            <a href="/"><img className={"logo"} src="../logo-branco.png"  alt="Tmenu"/></a>
+        </>
+    );
+}
+
+export default Logo
