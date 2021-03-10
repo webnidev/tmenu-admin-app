52246f003038be3ebeef00f93e6e2a7ecf59f810
Barroso Filho <barrosofilho.ti@gmail.com>
2021-03-10 06:02:15 -0300

    UsersIndex.js

--- src/components/Users/Usersindex.js
+++ src/components/Users/Usersindex.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { Typography } from "rmwc";
 
 const UsersIndex = () => {
@@ -14,4 +14,4 @@ const UsersIndex = () => {
     )
 }
 
-export default UsersIndex
+export default UsersIndex;
